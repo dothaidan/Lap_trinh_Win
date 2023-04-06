286aeed8dfb94bef985333c0afd15add8d83a5c3
ADMIN <ADMIN@DESKTOP-1BFS3B5>
2023-04-06 12:17:04 +0700

    Add project files.

---
+++ QuanLyQuanCafe/fLogin.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace QuanLyQuanCafe
+{
+    partial class fLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.But_close = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TeB_password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.But_login = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.But_close);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.But_login);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 478);
+            this.panel1.TabIndex = 0;
+            this.panel1.TabStop = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(64, 357);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(406, 37);
+            this.splitContainer1.SplitterDistance = 40;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = global::QuanLyQuanCafe.Properties.Resources.logo_flogin;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(3, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(359, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Username";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(22, 90);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(460, 73);
+            this.panel3.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuanLyQuanCafe.Properties.Resources.logo_flogin;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(42, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // But_close
+            // 
+            this.But_close.BackColor = System.Drawing.Color.DarkGray;
+            this.But_close.FlatAppearance.BorderSize = 0;
+            this.But_close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.But_close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.But_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.But_close.ForeColor = System.Drawing.Color.White;
+            this.But_close.Location = new System.Drawing.Point(268, 281);
+            this.But_close.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.But_close.Name = "But_close";
+            this.But_close.Size = new System.Drawing.Size(94, 28);
+            this.But_close.TabIndex = 3;
+            this.But_close.Text = "Thoát";
+            this.But_close.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.TeB_password);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(23, 169);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(459, 70);
+            this.panel2.TabIndex = 0;
+            // 
+            // TeB_password
+            // 
+            this.TeB_password.Location = new System.Drawing.Point(131, 18);
+            this.TeB_password.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.TeB_password.Name = "TeB_password";
+            this.TeB_password.Size = new System.Drawing.Size(297, 22);
+            this.TeB_password.TabIndex = 1;
+            this.TeB_password.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(59, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // But_login
+            // 
+            this.But_login.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.But_login.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.But_login.BackColor = System.Drawing.Color.DarkGray;
+            this.But_login.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.But_login.FlatAppearance.BorderSize = 0;
+            this.But_login.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.But_login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkGray;
+            this.But_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.But_login.ForeColor = System.Drawing.Color.White;
+            this.But_login.Location = new System.Drawing.Point(135, 281);
+            this.But_login.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.But_login.Name = "But_login";
+            this.But_login.Size = new System.Drawing.Size(94, 28);
+            this.But_login.TabIndex = 2;
+            this.But_login.Text = "Đăng nhập";
+            this.But_login.UseVisualStyleBackColor = false;
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(519, 493);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Panel panel3;
+        private TextBox TeB_password;
+        private Label label2;
+        private Button But_login;
+        private Button But_close;
+        private PictureBox pictureBox1;
+        private SplitContainer splitContainer1;
+        private PictureBox pictureBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ QuanLyQuanCafe/fLogin.cs
@@ -0,0 +1,13 @@
+using System.Xml.Linq;
+
+namespace QuanLyQuanCafe
+{
+    public partial class fLogin : Form
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
