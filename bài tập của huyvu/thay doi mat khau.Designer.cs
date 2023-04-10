aaa00a92c68da9ee71ee26d924ba564d70a6db58
Huy Vũ <126972427+HuyVu003@users.noreply.github.com>
2023-04-10 13:32:01 +0700

    Add files via upload

---
+++ bài tập của huyvu/thay doi mat khau.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace thay_doi_mat_khau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab_matkhaumoi = new System.Windows.Forms.Label();
+            this.lab_nhaplaimatkhau = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_dongy = new System.Windows.Forms.Button();
+            this.btn_huybo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lab_matkhaucu = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(272, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thay đổi mật khẩu";
+            // 
+            // lab_matkhaumoi
+            // 
+            this.lab_matkhaumoi.AutoSize = true;
+            this.lab_matkhaumoi.Location = new System.Drawing.Point(3, 9);
+            this.lab_matkhaumoi.Name = "lab_matkhaumoi";
+            this.lab_matkhaumoi.Size = new System.Drawing.Size(89, 16);
+            this.lab_matkhaumoi.TabIndex = 2;
+            this.lab_matkhaumoi.Text = "Mật khẩu mới:";
+            this.lab_matkhaumoi.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lab_nhaplaimatkhau
+            // 
+            this.lab_nhaplaimatkhau.AutoSize = true;
+            this.lab_nhaplaimatkhau.Location = new System.Drawing.Point(3, 11);
+            this.lab_nhaplaimatkhau.Name = "lab_nhaplaimatkhau";
+            this.lab_nhaplaimatkhau.Size = new System.Drawing.Size(117, 16);
+            this.lab_nhaplaimatkhau.TabIndex = 3;
+            this.lab_nhaplaimatkhau.Text = "Nhập lại mật khẩu:";
+            this.lab_nhaplaimatkhau.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.lab_matkhaumoi);
+            this.panel2.Location = new System.Drawing.Point(79, 159);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(274, 71);
+            this.panel2.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox3);
+            this.panel3.Controls.Add(this.lab_nhaplaimatkhau);
+            this.panel3.Location = new System.Drawing.Point(79, 248);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(274, 71);
+            this.panel3.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 30);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(250, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // btn_dongy
+            // 
+            this.btn_dongy.Location = new System.Drawing.Point(145, 334);
+            this.btn_dongy.Name = "btn_dongy";
+            this.btn_dongy.Size = new System.Drawing.Size(75, 34);
+            this.btn_dongy.TabIndex = 7;
+            this.btn_dongy.Text = "Đồng ý";
+            this.btn_dongy.UseVisualStyleBackColor = true;
+            this.btn_dongy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_huybo
+            // 
+            this.btn_huybo.Location = new System.Drawing.Point(260, 334);
+            this.btn_huybo.Name = "btn_huybo";
+            this.btn_huybo.Size = new System.Drawing.Size(75, 34);
+            this.btn_huybo.TabIndex = 8;
+            this.btn_huybo.Text = "Hủy bỏ";
+            this.btn_huybo.UseVisualStyleBackColor = true;
+            this.btn_huybo.Click += new System.EventHandler(this.btn_huybo_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::thay_doi_mat_khau.Properties.Resources._268712273_614883722947399_7026318691479135217_n;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(226, 241);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lab_matkhaucu
+            // 
+            this.lab_matkhaucu.AutoSize = true;
+            this.lab_matkhaucu.Location = new System.Drawing.Point(3, 9);
+            this.lab_matkhaucu.Name = "lab_matkhaucu";
+            this.lab_matkhaucu.Size = new System.Drawing.Size(81, 16);
+            this.lab_matkhaucu.TabIndex = 1;
+            this.lab_matkhaucu.Text = "Mật khẩu cũ:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lab_matkhaucu);
+            this.panel1.Location = new System.Drawing.Point(79, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(274, 66);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_huybo);
+            this.Controls.Add(this.btn_dongy);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab_matkhaumoi;
+        private System.Windows.Forms.Label lab_nhaplaimatkhau;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_dongy;
+        private System.Windows.Forms.Button btn_huybo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lab_matkhaucu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
