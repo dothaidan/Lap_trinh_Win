fedf2869abf1966c53f62baf452223cbcd0e631f
dothaidan103@gmail.com <dothaidan103@gmail.com>
2023-05-04 22:00:00 +0700

    cập nhập tổng tiền

--- QuanLyQuanCafe/fMenu.Designer.cs
+++ QuanLyQuanCafe/fMenu.Designer.cs
@@ -58,18 +58,18 @@
             this.panel2.Controls.Add(this.comboBox3);
             this.panel2.Controls.Add(this.comboBox2);
             this.panel2.Controls.Add(this.but_AddFood);
-            this.panel2.Location = new System.Drawing.Point(16, 11);
+            this.panel2.Location = new System.Drawing.Point(16, 12);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(384, 68);
+            this.panel2.Size = new System.Drawing.Size(384, 73);
             this.panel2.TabIndex = 1;
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(303, 24);
+            this.numericUpDown2.Location = new System.Drawing.Point(303, 26);
             this.numericUpDown2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(81, 23);
+            this.numericUpDown2.Size = new System.Drawing.Size(81, 22);
             this.numericUpDown2.TabIndex = 5;
             // 
             // comboBox3
@@ -85,10 +85,10 @@
             "7",
             "8",
             "9"});
-            this.comboBox3.Location = new System.Drawing.Point(3, 35);
+            this.comboBox3.Location = new System.Drawing.Point(3, 37);
             this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(206, 23);
+            this.comboBox3.Size = new System.Drawing.Size(206, 24);
             this.comboBox3.TabIndex = 3;
             // 
             // comboBox2
@@ -107,7 +107,7 @@
             this.comboBox2.Location = new System.Drawing.Point(3, 2);
             this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(206, 23);
+            this.comboBox2.Size = new System.Drawing.Size(206, 24);
             this.comboBox2.TabIndex = 2;
             // 
             // but_AddFood
@@ -115,7 +115,7 @@
             this.but_AddFood.Location = new System.Drawing.Point(215, 0);
             this.but_AddFood.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.but_AddFood.Name = "but_AddFood";
-            this.but_AddFood.Size = new System.Drawing.Size(82, 62);
+            this.but_AddFood.Size = new System.Drawing.Size(82, 66);
             this.but_AddFood.TabIndex = 0;
             this.but_AddFood.Text = "Thêm món";
             this.but_AddFood.UseVisualStyleBackColor = true;
@@ -127,15 +127,15 @@
             this.panel3.Controls.Add(this.but_Thanhtoan);
             this.panel3.Controls.Add(this.but_Giamgia);
             this.panel3.Controls.Add(this.but_Chuyenban);
-            this.panel3.Location = new System.Drawing.Point(16, 309);
+            this.panel3.Location = new System.Drawing.Point(16, 330);
             this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(384, 68);
+            this.panel3.Size = new System.Drawing.Size(384, 73);
             this.panel3.TabIndex = 2;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(150, 36);
+            this.numericUpDown1.Location = new System.Drawing.Point(150, 38);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             100000000,
@@ -143,7 +143,7 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(93, 23);
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 22);
             this.numericUpDown1.TabIndex = 4;
             // 
             // comboBox4
@@ -159,28 +159,34 @@
             "7",
             "8",
             "9"});
-            this.comboBox4.Location = new System.Drawing.Point(3, 35);
+            this.comboBox4.Location = new System.Drawing.Point(3, 37);
             this.comboBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(104, 23);
+            this.comboBox4.Size = new System.Drawing.Size(104, 24);
             this.comboBox4.TabIndex = 3;
             // 
             // but_Thanhtoan
             // 
-            this.but_Thanhtoan.Location = new System.Drawing.Point(298, 2);
+            this.but_Thanhtoan.BackColor = System.Drawing.Color.RoyalBlue;
+            this.but_Thanhtoan.FlatAppearance.BorderSize = 0;
+            this.but_Thanhtoan.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Blue;
+            this.but_Thanhtoan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.but_Thanhtoan.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but_Thanhtoan.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.but_Thanhtoan.Location = new System.Drawing.Point(265, 2);
             this.but_Thanhtoan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.but_Thanhtoan.Name = "but_Thanhtoan";
-            this.but_Thanhtoan.Size = new System.Drawing.Size(81, 64);
+            this.but_Thanhtoan.Size = new System.Drawing.Size(114, 68);
             this.but_Thanhtoan.TabIndex = 2;
             this.but_Thanhtoan.Text = "Thanh toán";
-            this.but_Thanhtoan.UseVisualStyleBackColor = true;
+            this.but_Thanhtoan.UseVisualStyleBackColor = false;
             // 
             // but_Giamgia
             // 
             this.but_Giamgia.Location = new System.Drawing.Point(150, 2);
             this.but_Giamgia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.but_Giamgia.Name = "but_Giamgia";
-            this.but_Giamgia.Size = new System.Drawing.Size(93, 28);
+            this.but_Giamgia.Size = new System.Drawing.Size(93, 30);
             this.but_Giamgia.TabIndex = 1;
             this.but_Giamgia.Text = "Giảm giá";
             this.but_Giamgia.UseVisualStyleBackColor = true;
@@ -190,7 +196,7 @@
             this.but_Chuyenban.Location = new System.Drawing.Point(3, 2);
             this.but_Chuyenban.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.but_Chuyenban.Name = "but_Chuyenban";
-            this.but_Chuyenban.Size = new System.Drawing.Size(103, 28);
+            this.but_Chuyenban.Size = new System.Drawing.Size(103, 30);
             this.but_Chuyenban.TabIndex = 0;
             this.but_Chuyenban.Text = "Chuyển bàn";
             this.but_Chuyenban.UseVisualStyleBackColor = true;
@@ -198,10 +204,10 @@
             // panel4
             // 
             this.panel4.Controls.Add(this.lsvBill);
-            this.panel4.Location = new System.Drawing.Point(16, 84);
+            this.panel4.Location = new System.Drawing.Point(16, 90);
             this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(384, 220);
+            this.panel4.Size = new System.Drawing.Size(384, 235);
             this.panel4.TabIndex = 3;
             // 
             // lsvBill
@@ -214,7 +220,7 @@
             this.lsvBill.Location = new System.Drawing.Point(3, 2);
             this.lsvBill.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lsvBill.Name = "lsvBill";
-            this.lsvBill.Size = new System.Drawing.Size(377, 219);
+            this.lsvBill.Size = new System.Drawing.Size(377, 233);
             this.lsvBill.TabIndex = 0;
             this.lsvBill.UseCompatibleStateImageBehavior = false;
             this.lsvBill.View = System.Windows.Forms.View.Details;
@@ -243,12 +249,13 @@
             // 
             // fMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 384);
+            this.ClientSize = new System.Drawing.Size(425, 410);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "fMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- QuanLyQuanCafe/fMenu.cs
+++ QuanLyQuanCafe/fMenu.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,14 +32,22 @@ namespace QuanLyQuanCafe
         {
             lsvBill.Items.Clear();
             this.ListMenu = ListMenu;
-            foreach (Menu bill in ListMenu)
+            double TotalPrice = 0;
+            if (this.ListMenu.Count > 0)
             {
-                ListViewItem lsvItem = new ListViewItem(bill.Name.ToString());
-                lsvItem.SubItems.Add(bill.Count.ToString());
-                lsvItem.SubItems.Add(bill.Price.ToString());
-                lsvItem.SubItems.Add(bill.TotalPrice.ToString());
-                lsvBill.Items.Add(lsvItem);
+                foreach (Menu bill in ListMenu)
+                {
+                    ListViewItem lsvItem = new ListViewItem(bill.Name.ToString());
+                    lsvItem.SubItems.Add(bill.Count.ToString());
+                    lsvItem.SubItems.Add(bill.Price.ToString());
+                    lsvItem.SubItems.Add(bill.TotalPrice.ToString());
+                    TotalPrice+= bill.Price;
+                    lsvBill.Items.Add(lsvItem);
+                }
+                CultureInfo culture = new CultureInfo("vi-VN");
+                but_Thanhtoan.Text= TotalPrice.ToString("c", culture);
             }
+            else but_Thanhtoan.Text = "Thanh toán";
         }
 
     }
